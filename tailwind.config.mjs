82fed9aefda62b4e9bfec10385f2a78d5d49ea74
Kaveh Rafie <kaveh_rafie@yahoo.com>
2025-01-25 11:28:10 -0800

    footnote styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+import theme from "tailwindcss/defaultTheme";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            ".prose .footnotes li": {
+              fontSize: theme.fontSize.sm[0],
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  safelist: ["sr-only"],
+  safelist: ["sr-only", "text-sm", "text-xs"],
 };
